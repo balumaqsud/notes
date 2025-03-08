81a9b9d7eeeeca8599fd476744a53de47261bcb1
balumaqsud <ollobergan008@mail.ru>
2025-03-08 21:41:25 +0900

    appjs, views, public folders added

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 const express = require("express");
 const app = express();
+
+//middlewares
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//fronted engine
+app.set("view engine", "ejs");
+app.set("views", "views");
