bb1b75b7c01853eb859b15a6f0ed237517689376
balumaqsud <ollobergan008@mail.ru>
2025-03-18 13:09:21 +0900

    update is working but refresh needed

--- public/browser.js
+++ public/browser.js
@@ -1,72 +1,11 @@
-let note_id = null;
-const addListItmes = (item) => {
-  return `<li
-              class="list-group-itemp p-1 mb-3 list-group-item-indo d-flex align-items-center justify-content-between"
-            >
-              <div>
-                <h6 class="list_title m-0 ">${item.title}</h6>
-                <p class="list_body m-0 text-secondary">${item.body}</p>
-                <p style="font-size: 12px;">${item.createdAt}</p>
-              </div>
-              <div>
-                <button data-id="${item._id}" class="delete_button btn btn-light btn-sm">
-                  <img width="18px" height="22px" src="icons/trash.svg" />
-                </button>
-              </div>
-            </li>`;
-};
-// axios post and get mentods
-document.getElementById("create-form").addEventListener("submit", (e) => {
-  e.preventDefault();
-  let title_input = document.getElementById("form_input_title");
-  let body_input = document.getElementById("form_textarea_body");
-  let button = document.querySelector(".create_btn");
-
-  if (title_input.value !== "" || body_input.value !== "") {
-    axios
-      .post("/create-item", {
-        title: title_input.value,
-        body: body_input.value,
-      })
-      .then((response) => {
-        document
-          .getElementById("notes_ul")
-          .insertAdjacentHTML("beforeend", addListItmes(response.data));
-        title_input.value = "";
-        body_input.value = "";
-        title_input.focus();
-      })
-      .catch((error) =>
-        console.log(`something went wrong in axios.post-then with ${error}`)
-      );
-  } else {
-    alert("Please name the note! and write someting!");
-  }
-  if (button.textContent === "Save New") {
-    axios
-      .post("/update-item", {
-        id: note_id,
-        title: title_input.value,
-        body: body_input.value,
-      })
-      .then((response) => {
-        console.log("added");
-      })
-      .catch((err) => {
-        err;
-      });
-  }
-
-  //buttons
-  button.textContent = "Save";
-});
-
+let note_id = "";
 //operations
 document.addEventListener("click", (e) => {
   //list reading
   const li_item = e.target.closest(".list-group-item-indo");
-  note_id = li_item.getAttribute("data-id");
-  if (li_item && !e.target.closest(".delete_button")) {
+  const id = li_item.getAttribute("data-id");
+  note_id = id;
+  if (note_id) {
     const title = li_item.querySelector(".list_title").textContent.trim();
     const body = li_item.querySelector(".list_body").textContent.trim();
 
@@ -76,7 +15,7 @@ document.addEventListener("click", (e) => {
 
     //button
     const button = document.querySelector(".create_btn");
-    button.textContent = "Save New";
+    button.textContent = "Update";
   }
 
   // clear
@@ -105,4 +44,67 @@ document.addEventListener("click", (e) => {
       })
       .catch((err) => err);
   }
+});
+
+/////form
+const addListItmes = (item) => {
+  return `<li
+              class="list-group-itemp p-1 mb-3 list-group-item-indo d-flex align-items-center justify-content-between"
+            >
+              <div>
+                <h6 class="list_title m-0 ">${item.title}</h6>
+                <p class="list_body m-0 text-secondary">${item.body}</p>
+                <p style="font-size: 12px;">${item.createdAt}</p>
+              </div>
+              <div>
+                <button data-id="${item._id}" class="delete_button btn btn-light btn-sm">
+                  <img width="18px" height="22px" src="icons/trash.svg" />
+                </button>
+              </div>
+            </li>`;
+};
+// axios post and get mentods
+console.log("iddd", note_id);
+document.getElementById("create-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  let title_input = document.getElementById("form_input_title");
+  let body_input = document.getElementById("form_textarea_body");
+  let button = document.querySelector(".create_btn");
+
+  if (note_id) {
+    axios
+      .post("/update-item", {
+        id: note_id,
+        title: title_input.value,
+        body: body_input.value,
+      })
+      .then((response) => {
+        console.log("updated");
+      })
+      .catch((err) => {
+        err;
+      });
+  } else if (title_input.value !== "" || body_input.value !== "") {
+    axios
+      .post("/create-item", {
+        title: title_input.value,
+        body: body_input.value,
+      })
+      .then((response) => {
+        document
+          .getElementById("notes_ul")
+          .insertAdjacentHTML("beforeend", addListItmes(response.data));
+        title_input.value = "";
+        body_input.value = "";
+        title_input.focus();
+      })
+      .catch((error) =>
+        console.log(`something went wrong in axios.post-then with ${error}`)
+      );
+  } else {
+    alert("Please name the note! and write someting!");
+  }
+
+  //buttons
+  button.textContent = "Save";
 });
